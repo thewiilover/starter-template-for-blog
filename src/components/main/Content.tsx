ead8745f5f781b0a522770ecacf612d3f8152a3d
jn <jnoncode@gmail.com>
2024-11-10 16:04:59 +0900

    error: useSearchParams error

--- src/components/main/Content.tsx
+++ src/components/main/Content.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from "react";
+
 import PostList from "@/src/components/main/content/PostList";
 import CategoryBox from "@/src/components/main/content/CategoryBox";
 import Profile from "@/src/components/global/Profile";
@@ -6,9 +8,11 @@ export default function Content() {
   return (
     <div className="relative w-full flex justify-center">
       <div className=" w-full lg:w-[1024px] flex justify-between">
-        <div className="w-full lg:w-[700px]">
-          <PostList />
-        </div>
+        <Suspense fallback={<div></div>}>
+          <div className="w-full lg:w-[700px]">
+            <PostList />
+          </div>
+        </Suspense>
         <div className="hidden lg:block sticky top-[100px] lg:w-[320px] h-[600px] px-5 py-8">
           <Profile />
           <CategoryBox />
